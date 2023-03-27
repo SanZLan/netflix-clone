99db413da66692e9fa0e93c09118b297da0d3303
SanZLan <szuluagalangton@hotmail.com>
2023-03-27 01:49:42 -0500

    Finished profiles page and sign in sequence

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/auth",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+}
 
 export default function Home() {
+    const { data: user } = useCurrentUser();
+
     return (
         <>
             <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+            <p className="text-white">Logged in as: {user?.name}</p>
+            <p className="text-white">Email: {user?.email}</p>
+            <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+                Logout
+            </button>
         </>
     );
 }
